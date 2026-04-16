2f33396eda318c8ed96e3e1841d0cacfde664779
Mahin <mahin.mohiuddin18@outlook.com>
2026-04-16 16:33:19 +0100

    updated task2.py to correct the sales number

--- task2.py
+++ task2.py
@@ -10,9 +10,12 @@ allData = []
 for file in csvFiles:
     data = pandas.read_csv(file)
     data = data[data["product"] == "pink morsel"]
+    data["price"] = data["price"].str.replace('$', '').astype(float)
+    data["quantity"] = data["quantity"].astype(float)
     data["sales"] = data["quantity"] * data["price"]
     data = data[["sales" , "date" , "region"]]
     allData.append(data)
 
 result = pandas.concat(allData, ignore_index=True)
+result["sales"] = result["sales"].apply(lambda x: f"${x:.2f}")
 result.to_csv("combinedSales.csv", index=False)
